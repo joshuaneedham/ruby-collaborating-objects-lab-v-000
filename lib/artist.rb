810a95bd54a593baca0797f230e66f7cd3573a59
Joshua Needham <joshuabneedham@gmail.com>
2017-11-13 18:17:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,16 +13,11 @@ class Artist
     @@all << name
   end
 
-  def songs
-    @songs << song
+  def songs(name)
+    song = Song.new(name)
+    self.songs << song
     song.artist = self
   end
-
-  def add_song_by_name(name)
-  song = Song.new(name)
-  self.songs << song
-  song.artist = self
-end
 
   def self.song_count
     @song
