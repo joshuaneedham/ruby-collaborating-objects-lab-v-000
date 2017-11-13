f55fb546a996c4968eead098e50afd698e1fdb84
Joshua Needham <joshuabneedham@gmail.com>
2017-11-13 18:38:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,9 +16,11 @@ class Artist
     @songs << song
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
   end
+
+  
 
   def save
     @@all << self
